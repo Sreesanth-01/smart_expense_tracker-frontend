5eda40d02a952b5a27a18d48a5d93017bf7763d9
Sreesanth-01 <iamshanth7@gmail.com>
2026-01-27 12:17:41 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <p className='text-3xl font-bold underline'>Hello World</p>
+    </div>
+  )
+}
+
+export default App
