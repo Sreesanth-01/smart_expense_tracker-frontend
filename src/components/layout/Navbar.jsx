8dfdfbd4c6e3e23f1fc9466a21c2a16ddb8ee2d3
Sreesanth-01 <iamshanth7@gmail.com>
2026-01-31 00:10:28 +0530

    feat: implement protected route for dashboard

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Link} from 'react-router-dom'
+import AuthContext from '../../context/AuthContext'
 
 const Navbar = () => {
+  // const {logout} = useContext(AuthContext);
   return (
     <nav className='flex gap-4 p-4 bg-gray-800 text-white'>
       <Link to="/dashboard">Dashboard</Link>
       <Link to="/expenses">Expenses</Link>
       <Link to="/login">Login</Link>
       <Link to="/register">Register</Link>
+
+      {/* <button onClick={logout}>Logout</button> */}
     </nav>
   )
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Login from "../pages/login";
 import Register from "../pages/register";
 import Dashboard from "../pages/dashboard";
 import Expenses from "../pages/expenses";
+import ProtectedRoute from "./ProtectedRoute";
 
 
 const AppRoutes = () =>{
@@ -10,7 +11,11 @@ const AppRoutes = () =>{
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />}/>
-            <Route path="/dashboard" element={<Dashboard />} />
+
+            <Route path="/dashboard" element={<ProtectedRoute>
+                                                <Dashboard />
+                                            </ProtectedRoute>} />
+                                            
             <Route path="/expenses" element={<Expenses />} />
         </Routes>
     )
