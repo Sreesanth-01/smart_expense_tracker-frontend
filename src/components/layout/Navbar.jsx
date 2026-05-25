4857ac4ee32b4c9e7edc81a1de0eb1eac25ee921
Sreesanth-01 <iamshanth7@gmail.com>
2026-05-25 17:24:12 +0530

    fix: Logo and title

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import AuthContext from '../../context/AuthContext'
-import logo from '../../assets/logo.png'
+import logo from '/logo.png'
 
 
 const Navbar = () => {
